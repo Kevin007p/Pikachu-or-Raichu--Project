2a1beaaea92a522d2794ec2a47052ece626c6581
Kevin007p <kkevinpatel07@gmail.com>
2024-09-11 23:44:43 -0400

    Fixed an error in /entity/config_entity.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from src.cnnClassifier import logger
-from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
